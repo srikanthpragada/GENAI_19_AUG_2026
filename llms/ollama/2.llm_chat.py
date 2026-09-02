341173ef1bf40f9ed1b6f9bc27b69183f73fb138
Srikanth Pragada <srikanthpragada@gmail.com>
2026-09-02 09:00:48 +0530

    02-SEP

--- llms/ollama/2.llm_chat.py
+++ llms/ollama/2.llm_chat.py
@@ -1,6 +1,6 @@
 from langchain_ollama import ChatOllama
  
 llm = ChatOllama(model="llama3.2:latest")
-response = llm.invoke("Who is Guido Van Rossum?")
+response = llm.invoke("When did world war 1 start?")
 print(response.content)
  
